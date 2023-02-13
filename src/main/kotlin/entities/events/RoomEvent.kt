8a033c26cbe61b3c6d9066695ce5c0280b6b88bb
Andrea Acampora <andrea.acampora@studio.unibo.it>
2023-02-13 22:52:00 +0100

    feat(events): create room event

---
+++ src/main/kotlin/entities/events/RoomEvent.kt
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package entities.events
+
+/**
+ * The event of change of humidity in a room of the operating block.
+ */
+data class RoomEvent<E : Any> (
+
+    override val key: String = "ROOM_EVENT",
+    /**
+     * The room ID.
+     */
+    val roomId: String,
+
+    override val data: E,
+
+) : Event<E>
