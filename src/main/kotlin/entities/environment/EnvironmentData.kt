aa0ff7cd3ede9fdd4506d1c928937d30e8cff2ba
Andrea Acampora <andrea.acampora@studio.unibo.it>
2023-02-13 22:43:21 +0100

    feat: create humidity value object

---
+++ src/main/kotlin/entities/environment/EnvironmentData.kt
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package entities.environment
+
+/**
+ * The object with all the environment data for the operating block rooms events.
+ */
+object EnvironmentData {
+
+    /**
+     * The humidity of the room.
+     * @param humidityPercentage the percentage of humidity.
+     */
+    data class Humidity(val humidityPercentage: Int)
+
+}
