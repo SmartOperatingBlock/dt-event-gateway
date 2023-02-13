2294769324344a04505220aa39b66ee5643091bf
Andrea Acampora <andrea.acampora@studio.unibo.it>
2023-02-13 22:38:13 +0100

    feat: create event interface

---
+++ src/main/kotlin/entities/events/Event.kt
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package entities.events
+
+/**
+ * The interface of a generic event.
+ */
+interface Event<E : Any> {
+
+    /**
+     * The key of the event. Every type of event has its unique key.
+     */
+    val key: String
+
+    /**
+     * The data of the event.
+     */
+    val data: E
+}
