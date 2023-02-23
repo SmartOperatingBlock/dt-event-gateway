781fe29b5c3966e78d89ccc0f5972d9ec7f47f52
Andrea Acampora <andrea.acampora@studio.unibo.it>
2023-02-23 22:25:44 +0100

    chore: add parser to event parser

--- src/main/kotlin/infrastructure/digitaltwins/parser/DTEventParser.kt
+++ src/main/kotlin/infrastructure/digitaltwins/parser/DTEventParser.kt
@@ -13,7 +13,9 @@ import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import entities.events.EmptyEvent
 import entities.events.Event
+import infrastructure.digitaltwins.events.LifecycleEvents.LifecycleEvent
 import infrastructure.digitaltwins.events.RelationshipEvents.RelationshipEvent
+import infrastructure.digitaltwins.events.TwinProperties.DTEventTypes.CREATE
 import infrastructure.digitaltwins.events.TwinProperties.DTEventTypes.RELATIONSHIP_CREATE
 import infrastructure.digitaltwins.events.TwinProperties.DTEventTypes.RELATIONSHIP_DELETE
 import infrastructure.digitaltwins.events.TwinProperties.DTEventTypes.UPDATE
@@ -26,6 +28,7 @@ class DTEventParser : EventParser<String> {
 
     private val updateEventParser = UpdateEventParser()
     private val relationshipEventParser = RelationshipEventParser()
+    private val lifecycleEventParser = LifecycleEventParser()
 
     override fun parseEvent(inputEvent: String): Event<Any> {
         val mapper = jacksonObjectMapper()
@@ -34,6 +37,11 @@ class DTEventParser : EventParser<String> {
             UPDATE.type -> {
                 updateEventParser.manageEvent(
                     mapper.readValue(inputEvent, UpdateTwinEvent::class.java)
+                )
+            }
+            CREATE.type -> {
+                lifecycleEventParser.manageEvent(
+                    mapper.readValue(inputEvent.replace("$", ""), LifecycleEvent::class.java)
                 )
             }
             RELATIONSHIP_CREATE.type -> {
