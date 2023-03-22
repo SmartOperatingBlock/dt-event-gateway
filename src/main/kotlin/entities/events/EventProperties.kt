433d2c791d2bfe998f898fb48192e72ac5ec83e2
Andrea Acampora <andrea.acampora@studio.unibo.it>
2023-03-22 10:56:43 +0100

    style: remove sonarcloud code smells

--- src/main/kotlin/entities/events/EventProperties.kt
+++ src/main/kotlin/entities/events/EventProperties.kt
@@ -11,24 +11,36 @@ package entities.events
 /** Module with some event properties. */
 object EventProperties {
 
+    /** The name of the topic for process events. */
+    const val processEvents = "process-events"
+
+    /** The name of the topic for room events. */
+    const val roomEvents = "room-events"
+
+    /** The name of the topic for tracking events. */
+    const val trackingEvents = "tracking-events"
+
+    /** The name of the topic for booking events. */
+    const val bookingEvents = "surgery-booking-events"
+
     /** The keys of the events and the [topicName] on which publish the event . */
     enum class EventKey(val topicName: String) {
-        MEDICAL_DEVICE_USAGE_EVENT("process-events"),
-        MEDICAL_TECHNOLOGY_USAGE_EVENT("process-events"),
-        TEMPERATURE_EVENT("room-events"),
-        HUMIDITY_EVENT("room-events"),
-        LUMINOSITY_EVENT("room-events"),
-        PRESENCE_EVENT("room-events"),
-        PATIENT_ON_OB_EVENT("process-events"),
-        PATIENT_BODY_TEMPERATURE_UPDATE_EVENT("process-events"),
-        PATIENT_DIASTOLIC_PRESSURE_UPDATE_EVENT("process-events"),
-        PATIENT_SYSTOLIC_PRESSURE_UPDATE_EVENT("process-events"),
-        PATIENT_RESPIRATORY_RATE_UPDATE_EVENT("process-events"),
-        PATIENT_SATURATION_UPDATE_EVENT("process-events"),
-        PATIENT_HEARTBEAT_UPDATE_EVENT("process-events"),
+        MEDICAL_DEVICE_USAGE_EVENT(processEvents),
+        MEDICAL_TECHNOLOGY_USAGE_EVENT(processEvents),
+        TEMPERATURE_EVENT(roomEvents),
+        HUMIDITY_EVENT(roomEvents),
+        LUMINOSITY_EVENT(roomEvents),
+        PRESENCE_EVENT(roomEvents),
+        PATIENT_ON_OB_EVENT(processEvents),
+        PATIENT_BODY_TEMPERATURE_UPDATE_EVENT(processEvents),
+        PATIENT_DIASTOLIC_PRESSURE_UPDATE_EVENT(processEvents),
+        PATIENT_SYSTOLIC_PRESSURE_UPDATE_EVENT(processEvents),
+        PATIENT_RESPIRATORY_RATE_UPDATE_EVENT(processEvents),
+        PATIENT_SATURATION_UPDATE_EVENT(processEvents),
+        PATIENT_HEARTBEAT_UPDATE_EVENT(processEvents),
         EMPTY_EVENT(""),
-        PATIENT_TRACKED_EVENT("process-events"),
-        TRACKING_EVENT("tracking-events"),
-        SURGERY_BOOKINGS_EVENT("surgery-bookings-events")
+        PATIENT_TRACKED_EVENT(processEvents),
+        TRACKING_EVENT(trackingEvents),
+        SURGERY_BOOKINGS_EVENT(bookingEvents)
     }
 }
